39d663102fc88fc66f03399ced712e5194064203
Majaz <majazulhaque123@gmail.com>
2023-05-20 12:50:51 +0530

    Increasing and decreasing the item value using setState(shallow merging & prevState concept

--- src/CartItem.js
+++ src/CartItem.js
@@ -12,8 +12,35 @@ class CartItem extends React.Component{
         // this.increaseQuantity = this.increaseQuantity.bind(this);
     }
     increaseQuantity = () => {
-        this.state.qty += 1;
-        console.log("Increase qty:",this.state.qty);
+        
+        // this.state.qty += 1;
+        // console.log("Increase qty:",this.state.qty);
+        //shallow merging
+        this.setState({
+            qty: this.state.qty + 1
+        },()=>{
+            console.log("Increase qty:",this.state.qty);
+        });
+    }
+    decreaseQuantity = () => {
+        const {qty} = this.state;
+        if(qty === 0){
+            return;
+        }
+        // this.state.qty -= 1;
+        // console.log("Decrease qty:",this.state.qty);
+        // if prevState is used
+        this.setState((prevState) => {
+            return {
+                qty: prevState.qty - 1
+            }
+        },()=>{
+            console.log("Decrease qty:",this.state.qty);
+        });
+    }
+    deleteQuantity = () =>{
+        this.state.qty = 0;
+        console.log("Delete qty:",this.state.qty);
     }
     render(){
         const {price,title,qty} = this.state;
@@ -29,8 +56,8 @@ class CartItem extends React.Component{
                     <div className="cart-item-actions">
                         {/*Button*/}
                         <img src="../image/plus.png" alt="increase" className="action-icons" onClick={this.increaseQuantity}/>
-                        <img src="../image/minuss.png" alt="decrease" className="action-icons"/>
-                        <img src="../image/trashs.png" alt="delete" className="action-icons"/>
+                        <img src="../image/minuss.png" alt="decrease" className="action-icons" onClick={this.decreaseQuantity}/>
+                        <img src="../image/trashs.png" alt="delete" className="action-icons" onClick={this.deleteQuantity}/>
                     </div>
                 </div>
             </div>
