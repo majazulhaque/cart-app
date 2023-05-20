2773a4845261cda971990c2088b56bdfa1846934
Majaz <majazulhaque123@gmail.com>
2023-05-20 16:06:01 +0530

    Delete products from carts and much more about props

--- src/Cart.js
+++ src/Cart.js
@@ -2,12 +2,76 @@ import React from "react";
 import CartItem from "./CartItem";
 
 class Cart extends React.Component{
+    constructor(){
+        super();
+        this.state = {
+            products:[
+                {
+                    price: 999,
+                    title: 'Phone',
+                    qty: 1,
+                    img: '',
+                    id:1
+                },
+                {
+                    price: 99,
+                    title: 'Charger',
+                    qty: 1,
+                    img: '',
+                    id:2
+                },
+                {
+                    price: 9,
+                    title: 'Band',
+                    qty: 1,
+                    img: '',
+                    id:3
+                }
+            ]
+        }
+        // this.increaseQuantity = this.increaseQuantity.bind(this);
+    }
+    handleIncreaseQuantity = (product) =>{
+        const {products} = this.state;
+        const index = products.indexOf(product);
+        products[index].qty += 1;
+        this.setState({
+            products: products
+        });
+    }
+    handleDecreaseQuantity = (product) =>{
+        const {products} = this.state;
+        const index = products.indexOf(product);
+        if(products[index].qty == 0){
+            return;
+        }
+        products[index].qty -= 1;
+        this.setState({
+            proudcts:products
+        });
+    }
+    handleDeleteQuantity = (id) =>{
+        const {products} = this.state;
+        const items = products.filter((item) => item.id !== id);
+        this.setState({
+            products: items
+        });
+    }
     render(){
+        const {products} = this.state;
         return (
             <div className="cart">
-                <CartItem/>
-                <CartItem/>
-                <CartItem/>
+                {products.map((product) => {
+                    return (
+                        <CartItem product ={product}
+                        key={product.id}
+                        onIncreaseQty={this.handleIncreaseQuantity}
+                        onDecreaseQty={this.handleDecreaseQuantity}
+                        onDeleteQty={this.handleDeleteQuantity}
+                        />
+                
+                    )
+                })}
             </div>
         )
     }
--- src/CartItem.js
+++ src/CartItem.js
@@ -1,48 +1,11 @@
 import React from "react";
 
 class CartItem extends React.Component{
-    constructor(){
-        super();
-        this.state = {
-            price: 999,
-            title: 'Phone',
-            qty: 1,
-            img: ''
-        }
-        // this.increaseQuantity = this.increaseQuantity.bind(this);
-    }
-    increaseQuantity = () => {
-        // this.state.qty += 1;
-        // console.log("Increase qty:",this.state.qty);
-        //shallow merging
-        this.setState({
-            qty: this.state.qty + 1
-        },()=>{
-            console.log("Increase qty:",this.state.qty);
-        });
-    }
-    decreaseQuantity = () => {
-        const {qty} = this.state;
-        if(qty === 0){
-            return;
-        }
-        // this.state.qty -= 1;
-        // console.log("Decrease qty:",this.state.qty);
-        // if prevState is used
-        this.setState((prevState) => {
-            return {
-                qty: prevState.qty - 1
-            }
-        },()=>{
-            console.log("Decrease qty:",this.state.qty);
-        });
-    }
-    deleteQuantity = () =>{
-        this.state.qty = 0;
-        console.log("Delete qty:",this.state.qty);
-    }
+    
+   
     render(){
-        const {price,title,qty} = this.state;
+        const {price,title,qty} = this.props.product;
+        const {product,onIncreaseQty,onDecreaseQty,onDeleteQty} = this.props;
         return (
             <div className="cart-item">
                 <div className="left-block">
@@ -54,9 +17,9 @@ class CartItem extends React.Component{
                     <div style={{color:'#777'}}>Qty: {qty}</div>
                     <div className="cart-item-actions">
                         {/*Button*/}
-                        <img src="../image/plus.png" alt="increase" className="action-icons" onClick={this.increaseQuantity}/>
-                        <img src="../image/minuss.png" alt="decrease" className="action-icons" onClick={this.decreaseQuantity}/>
-                        <img src="../image/trashs.png" alt="delete" className="action-icons" onClick={this.deleteQuantity}/>
+                        <img src="../image/plus.png" alt="increase" className="action-icons" onClick={()=>{onIncreaseQty(product)}}/>
+                        <img src="../image/minuss.png" alt="decrease" className="action-icons" onClick={()=>{onDecreaseQty(product)}}/>
+                        <img src="../image/trashs.png" alt="delete" className="action-icons" onClick={()=>{onDeleteQty(product.id)}}/>
                     </div>
                 </div>
             </div>
